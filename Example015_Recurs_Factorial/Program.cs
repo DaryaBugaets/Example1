c76b97540f7aedce0e8858ec3a497ca0b4ba5a42
Darya Bugaets <40bugaets@mail.ru>
2023-03-14 22:05:51 +0300

    15

--- Example015_Recurs_Factorial/Program.cs
+++ Example015_Recurs_Factorial/Program.cs
@@ -1,9 +1,9 @@
 ﻿double Factorial (int n)
 {
-    if (n == 1) return 1;
+    if (n <= 1) return 1;
     else return n * Factorial(n-1);       
 }
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < 10; i++)
 {
     Console.WriteLine($"{i}!={Factorial(i)}");
 }
